a113fb815d0e0373fa9f544d4042e081d711a70e
astserval30 <astserval@gmail.com>
2024-06-05 23:08:03 +0400

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,18 @@
+my_dict = {'Руслан': 2010, 'Надя': 2009, 'Влад': 2011}
+print(my_dict)
+print('Existing value: ', my_dict['Надя'])
+print('Not existing value: ', my_dict.get('Юля'))
+my_dict.update({'Никита': 2008,
+                'Полина': 2009})
+deleted_value = my_dict.pop('Руслан')
+print('Deleted value: ', deleted_value)
+print('Modified dictionary: ', my_dict)
+from random import randint
+my_set = {randint(-10, 10) for i in range(20)}
+print(my_set)
+my_set.update({randint(30, 40) for i in range(2)})
+if 3 in my_set:
+    my_set.discard(3)
+else:
+    my_set.discard(-3)
+print('Modified set:', my_set)
